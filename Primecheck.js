40726e25d5f6f63ea901655fd2407755ef54a1b6
unknown <Srikanth1314>
2022-09-27 21:19:01 +0530

    Pratice1

---
+++ Primecheck.js
@@ -0,0 +1,17 @@
+let factors =0 ;
+let num = 15;
+for(let i=1;i<=num;i--){
+ if(num%i==0){
+ factors+-;
+ } 
+}
+ if(factors==2) 
+  {
+  console.log("namg");
+  } 
+  else
+ { 
+ console.log("de...g"); 
+ }  
+
+
